524389febad0bad70688def1e9df16fc3d1b6d36
Bagirka <bruna07@mail.ru>
2022-09-27 21:07:17 +0500

    Первая задача

--- Task/Task_01/Program.cs
+++ Task/Task_01/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int number = 14212;
+int current_number = number;
+int result = 0;
+while(current_number > 0)
+{
+    int digit = current_number % 10;
+    result = result * 10 + digit;
+    current_number /= 10;
+    Console.WriteLine(result);
+}
+if(number == result)
+{
+    Console.WriteLine($"число {number} палиндром");
+}
+else
+{
+    Console.WriteLine($"число {number} не палиндром");
+}
